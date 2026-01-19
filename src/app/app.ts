2b9bd2508aafda74e2b0a79bc03c425bca1b5765
Morfeo <gastongomez2014@hotmail.com>
2026-01-19 18:03:26 -0300

    Conexion al footer hecha

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FooterComponent}  from './components/footer/footer'
 import { NavbarComponent } from './components/navbar/navbar'; 
 import { ProductDetailComponent } from './components/product-detail/product-detail'; // Importar
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [NavbarComponent, ProductDetailComponent], // Agregar aquí
+  imports: [NavbarComponent, ProductDetailComponent, FooterComponent], // Agregar aquí
   templateUrl: './app.html',
   styleUrls: ['./app.css']
 })
